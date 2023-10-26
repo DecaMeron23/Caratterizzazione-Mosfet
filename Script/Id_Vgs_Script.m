a6830aa8c2b08191dd9e73b55f31b41033cf120b
DecaMeron23 <e.meroni1@studenti.unibg.it>
2023-10-26 07:49:16 +0200

    Refactoring dei nomi  e aggiunta varaibili

--- Script/Id_Vgs_Script.m
+++ Script/Id_Vgs_Script.m
@@ -3,72 +3,77 @@
 %% inizializzazione
 clear; clc;
 
+% trovo la directory in cui ci troviamo
 fp = dir();
+% Lista dei file nella cartella
 fileInFolder = {fp.name};
+% verifichiamo se ci sono dei file nella cartella ( . e .. esclusi)
 if length(fileInFolder) <= 2
     error("Cartella vuota...")
 end
 
-NameFolder = fp.folder;
-[~ , Dispositivo] = fileparts(NameFolder);
+% prendo il nome completo della cartella (es: "C:\Dispositivi\N1_100-30")
+nameFolder = fp.folder;
+% prendo solo il nome della cartella (es: "N1_100-30")
+[~ , dispositivo] = fileparts(nameFolder);
 
 
-%nome del dispositivo
-device_type = Dispositivo(1);
+% Tipo del dispositivo
+device_type = dispositivo(1);
 
-File1 = "id-vgs.txt";
-File2 = "id-vgs-2.txt";
+% Nomi dei file contenenti il le Id, al variare di Vds, e Vgs
+file1 = "id-vgs.txt";
+file2 = "id-vgs-2.txt";
 
 % Carico i file
-id_Vgs_completo_1 = readmatrix(File1); 
-id_Vgs_completo_2 = readmatrix(File2);    
+id_Vgs_completo_1 = readmatrix(file1); 
+id_Vgs_completo_2 = readmatrix(file2);    
 % file composto da {Vg + (Id , Ig , Is , Iavdd , Igrd) * vd}
 % numero di colonne totali
-NUMERO_COLONNE_1 = length(id_Vgs_completo_1(1, :));
-NUMERO_COLONNE_2 = length(id_Vgs_completo_2(1, :));
-%seleziono gli indici di colonna contenenti Id (nel primo file non prendo
-%vd = 0)
-colonne_vg_id_1 = 7:5:NUMERO_COLONNE_1;
-colonne_vg_id_2 = 7:5:NUMERO_COLONNE_2;
+NUM_COLONNE_TOT_1 = length(id_Vgs_completo_1(1, :));
+NUM_COLONNE_TOT_2 = length(id_Vgs_completo_2(1, :));
+%seleziono gli indici di colonna contenenti Id (non prendiamo le Vd = 0)
+colonne_vg_id_1 = 7:5:NUM_COLONNE_TOT_1;
+colonne_vg_id_2 = 7:5:NUM_COLONNE_TOT_2;
 % estraggo le colonne con Id
-Id_1 = id_Vgs_completo_1(: , colonne_vg_id_1);
-Id_2 = id_Vgs_completo_2(: , colonne_vg_id_2);
+id_1 = id_Vgs_completo_1(: , colonne_vg_id_1);
+id_2 = id_Vgs_completo_2(: , colonne_vg_id_2);
 % estraggo le Vg (sono uguali per entrambi i file)
-Vg = id_Vgs_completo_1(: , 1);
+vg = id_Vgs_completo_1(: , 1);
 %faccio il merge dei file
-Id = [Id_2 , Id_1];
+id = [id_2 , id_1];
 
 % Valori di Vds in mV
-Vds = [10:10:100, 150:150:900];
+vds = [10:10:100, 150:150:900];
 
 % pulisco il Workspace
-clear Id_1 Id_2 NUMERO_COLONNE_1 NUMERO_COLONNE_2 id_Vgs_completo_1 id_Vgs_completo_2 File1 File2 fp fileInFolder;
+clear id_1 id_2 NUM_COLONNE_TOT_1 NUM_COLONNE_TOT_2 id_Vgs_completo_1 id_Vgs_completo_2 file1 file2 fp fileInFolder;
 
 
 
 %% Calcoliamo Gm
 
-gm1 = zeros(size(Id));
+gm1 = zeros(size(id));
 gm2 = gm1;
 
-for i=1:length(Vds)
-    gm1(:,i) = gradient(Id(:,i))./gradient(Vg);
+for i=1:length(vds)
+    gm1(:,i) = gradient(id(:,i))./gradient(vg);
 end
 
 gm2(1,:) = gm1(1,:);
 
-for i=1:length(Vds)
-    gm2(2:end,i) = gradient(Id(1:end-1,i))./gradient(Vg(2:end));
+for i=1:length(vds)
+    gm2(2:end,i) = gradient(id(1:end-1,i))./gradient(vg(2:end));
 end
 
 gm = (gm1+gm2)/2;
 
-for i=1:length(Vds)
+for i=1:length(vds)
     gm(:,i) = smooth(gm(:,i));
 end
 
 figure
-plot(Vg , gm .* 1e3)
+plot(vg , gm .* 1e3)
 
 % nome assi
 ylabel('$g_m$ [mS]','interpreter','latex')
@@ -78,8 +83,8 @@ xlabel('$V_{gs}$ [V]','interpreter','latex')
 title(device_type + " - $g_m$",'interpreter','latex')
 
 % creo la legenda
-for i = 1: length(Vds)
-    legend_text(i) = "Vds = " + (Vds(i))+ " mV"; 
+for i = 1: length(vds)
+    legend_text(i) = "Vds = " + (vds(i))+ " mV"; 
 end
 
 if device_type == "N"
@@ -149,37 +154,36 @@ clear incremento_Vg legend_text
 %valido per basse Vds
 
 % inizializzazione dei dati
-TCM_data = zeros(length(Id(:, 1)), length(Vds));
-Vth_data_TCM = zeros(length(Vds) , 1 );
+TCM_data = zeros(length(id(:, 1)), length(vds));
 
 % se il dispositivo è un p specchiamo verticalmente la gm 
 if(device_type=='P')
     gm = flipud(gm); % giusto fare flipud della gm? perchè poi noi andiamo a cercare il massimo 
 end
 
-for i=1:length(Vds)
-    TCM_data(:,i) = gradient(gm(:,i))./gradient(Vg);
+for i=1:length(vds)
+    TCM_data(:,i) = gradient(gm(:,i))./gradient(vg);
 end
 
 % Smooth della derivata
-for i=1:length(Vds)
+for i=1:length(vds)
     TCM_data(: , i) = smooth(TCM_data(: , i));
 end
 
 [TCM_Max, TCM_Indice] = max(TCM_data(1:201,:)); % valore e indice massimo di di TCM per Vgs<=700mV
 
-for i=1:length(Vds)
-    Vth_data_TCM(i, 1) = Vg(TCM_Indice(i));
+for i=1:length(vds)
+    vth_TCM_noFit(i, 1) = vg(TCM_Indice(i));
 end
 
 %Calcolo del massimo della funzione polinomiale che interpola i punti 
 % in un intorno di Vth calcolata con TCM a Vgs = 10 mV e di raggio 100 mV
 grado = 6; % grado della polinomiale
-coefficienti = zeros(length(Vds), grado+1);
-for i = 1:length(Vds)
+coefficienti = zeros(length(vds), grado+1);
+for i = 1:length(vds)
     indici_intervallo = TCM_Indice(i)-20 : TCM_Indice(i)+20;
-    intervallo_alta_ris = Vg(indici_intervallo(1)):0.0001:Vg(indici_intervallo(end));
-    coefficienti(i,:) = polyfit(Vg(indici_intervallo), TCM_data(indici_intervallo,i), grado);
+    intervallo_alta_ris = vg(indici_intervallo(1)) : 0.0001 : vg(indici_intervallo(end));
+    coefficienti(i,:) = polyfit(vg(indici_intervallo), TCM_data(indici_intervallo,i), grado);
     grafico(:,i) = polyval(coefficienti(i,:), intervallo_alta_ris);
     % se Vds = 10mv (i == 1) teniamo gli intervalli per fare il grafico
     % dopo il for
@@ -188,19 +192,20 @@ for i = 1:length(Vds)
         intervallo_vds_10mv_alta_ris = intervallo_alta_ris;
     end
 end
-[max_grafico, ind_grafico] = max(grafico); %massimo della polinomiale
+%massimo della polinomiale
+[max_grafico, ind_grafico] = max(grafico); 
 
-for i=1:length(Vds)
-    Vth_TCM(i) = intervallo_vds_10mv_alta_ris(ind_grafico(i));
+for i=1:length(vds)
+    vth_TCM(i) = intervallo_vds_10mv_alta_ris(ind_grafico(i));
 end
 
 figure
 hold on
 title("TCM")
-plot(Vg(intervallo_vds_10mv),TCM_data(intervallo_vds_10mv,1)); %grafico dati
-xline(Vth_data_TCM(1),"--","Color","r");  %Vth dati
+plot(vg(intervallo_vds_10mv),TCM_data(intervallo_vds_10mv,1)); %grafico dati
+xline(vth_TCM_noFit(1),"--","Color","r");  %Vth dati
 plot(intervallo_vds_10mv_alta_ris,grafico(:, 1)); %grafico polinomiale
-plot(Vth_TCM(1) , max_grafico(1) , "o") %minimo della polinomiale (Vth)
+plot(vth_TCM(1) , max_grafico(1) , "o") %minimo della polinomiale (Vth)
 legend("SDLM","Massimo di TCM","Fit di grado "+grado, "Massimo del fit")
 
 clear a b gm indici_intervallo;
@@ -215,87 +220,93 @@ clear a b gm indici_intervallo;
 
 % se il dispositivo è un p specchiamo verticalmente il log(Id) 
 if device_type == 'P'
-    log_Id = log(flipud(abs(Id)));
+    log_Id = log(flipud(abs(id)));
 else
-    log_Id = log(abs(Id));
+    log_Id = log(abs(id));
 end
 
 %Eseguiamo lo smooth
-for i=1:length(Vds)
+for i=1:length(vds)
     log_Id_smooth(: , i) = smooth(log_Id(: , i));
 end
 
 %Deriviamo rispetto Vgs
-for i = 1:length(Vds) 
-    SDLM_derivata(: , i) = gradient(log_Id_smooth(: , i)) ./ gradient(Vg);
+for i = 1:length(vds) 
+    SDLM_derivata(: , i) = gradient(log_Id_smooth(: , i)) ./ gradient(vg);
 end
 
 %Eseguiamo lo smooth della derivata
-for i=1:length(Vds)
+for i=1:length(vds)
     SDLM_derivata(: , i) = smooth(SDLM_derivata(: , i));
 end
 
 %Deriviamo la seconda volta
-for i= 1:length(Vds)
-    SDLM_derivata_2(: , i) = gradient(SDLM_derivata(:,i)) ./ gradient(Vg);
+for i= 1:length(vds)
+    SDLM_derivata_2(: , i) = gradient(SDLM_derivata(:,i)) ./ gradient(vg);
 end
 
 % per bassi valori di Vgs (da -0.3 a -0.2) sostituiamo i valori 
-spuriousRemoved = [ones(20,length(Vds))*200; SDLM_derivata_2(21:end,:)]; 
+spuriousRemoved = [ones(20,length(vds))*200; SDLM_derivata_2(21:end,:)]; 
 
 %Smooth della derivata seconda
-for i=1:length(Vds)
+for i=1:length(vds)
     SDLM_derivata_2(:, i) = smooth(spuriousRemoved(:,i));
 end
 
 [SDLM_Min, SDLM_Indice] = min(SDLM_derivata_2); % #modifica: SDLM_derivata_2 --> SDLM_derivata_2_smooth
 
-for i=1:length(Vds)
-    Vth_data_SDLM(i, 1) = Vg(SDLM_Indice(i));
+for i=1:length(vds)
+    vth_SDLM_noFit(i, 1) = vg(SDLM_Indice(i));
 end
 
 %Calcolo del minimo della funzione polinomiale che interpola i punti 
 % in un intorno di Vth calcolata con SDLM a Vgs = 900 mV e di raggio 100 mV
 grado = 6; % grado della polinomiale
-coefficienti = zeros(length(Vds), grado+1);
-for i = 1:length(Vds)
+coefficienti = zeros(length(vds), grado+1);
+for i = 1:length(vds)
     indici_intervallo = SDLM_Indice(i)-20 : SDLM_Indice(i)+20;
-    intervallo_alta_ris = Vg(indici_intervallo(1)):0.0001:Vg(indici_intervallo(end));
-    coefficienti(i,:) = polyfit(Vg(indici_intervallo), SDLM_derivata_2(indici_intervallo,i), grado);
+    intervallo_alta_ris = vg(indici_intervallo(1)):0.0001:vg(indici_intervallo(end));
+    coefficienti(i,:) = polyfit(vg(indici_intervallo), SDLM_derivata_2(indici_intervallo,i), grado);
     grafico(:,i) = polyval(coefficienti(i,:), intervallo_alta_ris);
+    % se Vds = 900mv (i == length(Vds)) teniamo gli intervalli per fare il grafico
+    % dopo il for
+    if(i == length(vds))
+        indici_intervallo_vds_900mv = indici_intervallo;
+        intervallo_vds_900mv_alta_ris = intervallo_alta_ris;
+    end
 end
 [min_grafico, ind_grafico] = min(grafico); %minimo della polinomiale
 
-for i=1:length(Vds)
-    Vth_SDLM(i) = intervallo_alta_ris(ind_grafico(i));
+for i=1:length(vds)
+    vth_SDLM(i) = intervallo_vds_900mv_alta_ris(ind_grafico(i));
 end
 
 figure
 hold on
 title("SDLM")
-plot(Vg(indici_intervallo),SDLM_derivata_2(indici_intervallo,end)) %grafico dati
-xline(Vth_data_SDLM(end),"--","Color","r"); %Vth dati
-plot(intervallo_alta_ris, grafico(:, end)); %grafico polinomiale
-plot(Vth_SDLM(end) , min_grafico(end) , "o") %minimo della polinomiale (Vth)
-legend( "SDLM", "Minimo di SDLM", "Fit di grado "+grado, "Minimo del fit");
+plot(vg(indici_intervallo_vds_900mv),SDLM_derivata_2(indici_intervallo_vds_900mv,end)) %grafico dati
+xline(vth_SDLM_noFit(end),"--","Color","r"); %Vth dati
+plot(intervallo_vds_900mv_alta_ris, grafico(:, end)); %grafico polinomiale
+plot(vth_SDLM(end) , min_grafico(end) , "o") %minimo della polinomiale (Vth)
+legend( "SDLM", "Minimo di SDLM", "Fit di grado "+ grado, "Minimo del fit");
 clear spuriousRemoved;
 
 %% Save File
 % verticalizzo Vds
-Vds_verticale = Vds';
+vds_verticale = vds';
 %creo una matrice contenente le Vth calcolate
-Vth =  array2table([Vds_verticale , round(Vth_TCM' , 6) , round(Vth_SDLM' , 6)]);
+vth =  array2table([vds_verticale , round(vth_TCM' , 6) , round(vth_SDLM' , 6)]);
 %Rinonimo le intestazioni
-Vth = renamevars(Vth , ["Var1", "Var2", "Var3"] , ["Vd" , "Vth_TCM", "Vth_SDLM"]);
+vth = renamevars(vth , ["Var1", "Var2", "Var3"] , ["Vd" , "Vth_TCM", "Vth_SDLM"]);
 
 % cd ..
 % 
 %  if ~exist("~\Vth")
 %     mkdir("Vth");
 %  end
-%  cd Vth;
+%  cd vth;
 
 %Salvo File nella cartella
-writetable( Vth, "Vth_"+ Dispositivo + ".txt",  "Delimiter", "\t");
+writetable( vth, "Vth_"+ dispositivo + ".txt",  "Delimiter", "\t");
 
 % ci muoviamo nella prossima cartella da analizzare
