797bf86bbff1880fe4569dc1b1953b9eb5375975
DecaMeron23 <e.meroni1@studenti.unibg.it>
2024-02-02 16:58:32 +0100

    Aggiunta Script e aggiornamento Vth
    Aggiunta funzione che calcola la Vth per tutte le misure irraggiate fino a quel momento, aggiornamento delle delta Vth, aggioramento script delta_vth il quale ora fa anche i plot, aggiunta funzione (nella cartella altri scipt) la quale estrae una tabella in .xml che contiene tutte le vth dei dispositivi e metodi un un file unico.

---
+++ Altri Script/EstraiVth.m
@@ -0,0 +1,76 @@
+%funzione che estrae per ogni metodo un file con tutte le vth dei i dispositivi
+function EstraiVth
+    directory = dir;
+    file = {directory.name};
+    
+    if ~(exist("tabelle" , "dir"))
+        mkdir tabelle
+    end
+    vth_file = {};
+    for i = 3 : length(file)
+    
+        if contains(string(file(i)), ".txt")
+             vth_file{end+1} = string(file(i));
+        end
+    end
+    
+    vth_file = sortVthFile(vth_file);
+    
+    dispositivi = ["100/30" "100/60" "100/180" "200/30" "200/60" "200/180" "600/30" "600/60" "600/180"];
+
+    % carichiamo i file
+    for i = 1: length(vth_file)
+        if ~isnumeric(vth_file{1, i})
+            matrice(: , i) = readmatrix(string(vth_file(i)));   
+        end
+    end
+
+    tabella = array2table( matrice , VariableNames=dispositivi);
+    tabella = addvars(tabella, ["Fit Lin" ;"TCM" ; "SDLM" ; "RM " ; "RM_FIT_PRE" ] , 'Before', 1 , 'NewVariableNames', 'Metodo');
+    
+    cd tabelle
+    writetable(tabella , "Vth" , FileType="spreadsheet");
+    cd ..
+end
+
+% funzione che ordina il cell array dei file Vth in base alla dimensione: prima
+% 100-30 poi 100-60, 100-180 poi con i 200 e i 600 (l'ultimo è il 600-180)
+function fileVth_sort = sortVthFile(fileVth)
+
+    fileVth_sort = {};
+
+    for i = fileVth
+        file = string(i);
+        file = char(file);
+        file_noEst = file(1:(end-4)); %togliamo l'estensione.
+    
+        [~ , W , L] = titoloPlot(file_noEst); % se file è cosi: Vth_P1-600-180 ci ritorna W = 600 e L = 0.18
+
+        if (W == 100)
+            if(L == 0.03)
+                fileVth_sort{1} = file;
+            elseif(L == 0.06)
+                fileVth_sort{2} = file;
+            elseif(L == 0.18)
+                fileVth_sort{3} = file;
+            end
+        elseif(W == 200)
+            if(L == 0.03)
+                fileVth_sort{4} = file;
+            elseif(L == 0.06)
+                fileVth_sort{5} = file;
+            elseif(L == 0.18)
+                fileVth_sort{6} = file;
+            end
+        elseif(W == 600)
+            if(L == 0.03)
+                fileVth_sort{7} = file;
+            elseif(L == 0.06)
+                fileVth_sort{8} = file;
+            elseif(L == 0.18)
+                fileVth_sort{9} = file;
+            end
+        end
+    end
+end
+
--- Script Statiche/Calcolo_Vth.m
+++ Script Statiche/Calcolo_Vth.m
@@ -3,85 +3,85 @@
 %% Impostare a 1 se la variabile preIrreggiamento se il dispositivo non è ancora stato irraggiato
 %% per ogni cartella serve avere i fle .txt delle misurazioni
 %% inizializzazione
-clear; clc;
-
-% abilitare i plot di verifica (si = 1, no = 0)  
-PLOT_ON = 0;
-
-% indichiamo se il dispositivo è pre irraggiamento
-preIrraggiamento = 0;
-
-if preIrraggiamento == 1
-    SPAN = 20;
-    GRADO = 6;
-elseif preIrraggiamento == 0
-    SPAN = 5;
-    GRADO = 6;
+function Calcolo_Vth()
+    
+    % abilitare i plot di verifica (si = 1, no = 0)  
+    PLOT_ON = 0;
+    
+    % indichiamo se il dispositivo è pre irraggiamento
+    preIrraggiamento = 0;
+    
+    if preIrraggiamento == 1
+        SPAN = 20;
+        GRADO = 6;
+    elseif preIrraggiamento == 0
+        SPAN = 5;
+        GRADO = 6;
+    end
+    
+    % trovo la directory in cui ci troviamo
+    fp = dir();
+    % Lista dei file nella cartella
+    fileInFolder = {fp.name};
+    % verifichiamo se ci sono dei file nella cartella ( . e .. esclusi)
+    if length(fileInFolder) <= 2
+        error("Cartella vuota...")
+    end
+    
+    
+    for i = 3 : length(fileInFolder)
+        dispositivo = char(fileInFolder(i));
+        if ((dispositivo(1) == 'N' || dispositivo(1) == 'P') && (dispositivo(3) == '-')&&  ...
+                ~contains(dispositivo,'nf'))
+    
+            disp(dispositivo + ":");
+    
+            if ~PLOT_ON
+                set(0, 'DefaultFigureVisible', 'off');
+            end
+    
+            vth_FIT = Vth.FIT_LIN(dispositivo , PLOT_ON);
+            vth_TCM= Vth.TCM(dispositivo , GRADO , PLOT_ON);
+            vth_SDLM = Vth.SDLM (dispositivo , GRADO , PLOT_ON);
+            vth_RM = Vth.RM(dispositivo , PLOT_ON);
+            vth_RM_fitEstratti = Vth.RM_Estremi_PreIrraggiamento(dispositivo , PLOT_ON);
+            
+            if ~PLOT_ON
+                set(0, 'DefaultFigureVisible', 'on');
+            end
+            
+            formato = '%5.5f';
+            
+    
+            vth_FIT = string(sprintf(formato, vth_FIT));
+            vth_TCM = string(sprintf(formato, vth_TCM));
+            vth_SDLM = string(sprintf(formato, vth_SDLM));
+            vth_RM = string(sprintf(formato, vth_RM));
+            vth_RM_fitEstratti = string(sprintf(formato, vth_RM_fitEstratti));
+            
+            vth = [vth_FIT , vth_TCM , vth_SDLM , vth_RM , vth_RM_fitEstratti];
+            
+            %% Save File
+            %Rinonimo le intestazioni
+    
+            vth = array2table(vth);
+            vth = renamevars(vth , ["vth1" , "vth2" "vth3" , "vth4" , "vth5"] , ["Lin_fit_Id", "Vth_TCM", "Vth_SDLM" ,"Vth_RM" , "Vth_RM_Fit_Pre"]);
+            
+            Cartella = "Vth";
+           
+            if ~exist(Cartella , "dir")
+                mkdir(Cartella);  
+            end
+            
+            cd(Cartella);
+             
+            
+            %Salvo File nella cartella
+            writetable( vth, "Vth_" + dispositivo + ".txt",  "Delimiter", "\t");
+    
+            cd ..
+            
+         end
+    end
+    
 end
-
-% trovo la directory in cui ci troviamo
-fp = dir();
-% Lista dei file nella cartella
-fileInFolder = {fp.name};
-% verifichiamo se ci sono dei file nella cartella ( . e .. esclusi)
-if length(fileInFolder) <= 2
-    error("Cartella vuota...")
-end
-
-
-for i = 3 : length(fileInFolder)
-    dispositivo = char(fileInFolder(i));
-    if ((dispositivo(1) == 'N' || dispositivo(1) == 'P') && (dispositivo(3) == '-')&&  ...
-            ~contains(dispositivo,'nf'))
-
-        disp(dispositivo + ":");
-
-        if ~PLOT_ON
-            set(0, 'DefaultFigureVisible', 'off');
-        end
-
-        vth_FIT = Vth.FIT_LIN(dispositivo , PLOT_ON);
-        vth_TCM= Vth.TCM(dispositivo , GRADO , PLOT_ON);
-        vth_SDLM = Vth.SDLM (dispositivo , GRADO , PLOT_ON);
-        vth_RM = Vth.RM(dispositivo , PLOT_ON);
-        vth_RM_fitEstratti = Vth.RM_Estremi_PreIrraggiamento(dispositivo , PLOT_ON);
-        
-        if ~PLOT_ON
-            set(0, 'DefaultFigureVisible', 'on');
-        end
-        
-        formato = '%5.5f';
-        
-
-        vth_FIT = string(sprintf(formato, vth_FIT));
-        vth_TCM = string(sprintf(formato, vth_TCM));
-        vth_SDLM = string(sprintf(formato, vth_SDLM));
-        vth_RM = string(sprintf(formato, vth_RM));
-        vth_RM_fitEstratti = string(sprintf(formato, vth_RM_fitEstratti));
-        
-        vth = [vth_FIT , vth_TCM , vth_SDLM , vth_RM , vth_RM_fitEstratti];
-        
-        %% Save File
-        %Rinonimo le intestazioni
-
-        vth = array2table(vth);
-        vth = renamevars(vth , ["vth1" , "vth2" "vth3" , "vth4" , "vth5"] , ["Lin_fit_Id", "Vth_TCM", "Vth_SDLM" ,"Vth_RM" , "Vth_RM_Fit_Pre"]);
-        
-        Cartella = "Vth";
-       
-        if ~exist(Cartella , "dir")
-            mkdir(Cartella);  
-        end
-        
-        cd(Cartella);
-         
-        
-        %Salvo File nella cartella
-        writetable( vth, "Vth_" + dispositivo + ".txt",  "Delimiter", "\t");
-
-        cd ..
-        
-     end
-end
-
-clear;
