32d365b4f4042c1261ee2f12e53a5da52fce39e9
DecaMeron23 <e.meroni1@studenti.unibg.it>
2024-03-13 09:44:53 +0100

    Modifica Delta_Gm
    Ora delta Gm prende in considerazione tutte le vds, e produce una tabella contenente tutte le delta al variare della dose subita e della vds

--- Script Statiche/ScriptPlot/delta_gm_percentuale.m
+++ Script Statiche/ScriptPlot/delta_gm_percentuale.m
@@ -1,41 +1,58 @@
-function  delta_gm_percentuale(dispositivo) %dispositivo = "200-30";
+function  delta_gm_percentuale(dispositivo) %dispositivo = "200-30" vds = 0.45
 
     [cartelle , tipologia]= estrazioneCartelle.getCartelle();
 
     grado = [0 5 50 100 200 600 1000];
-    delta = zeros(1 , 7);
+    valori_vds = [0.15 , 0.30 , 0.45 , 0.6 , 0.75 , 0.9];
+    delta = zeros(6 , 7);
     for i = 1: length(cartelle)
-        
+        disp("Grado = " + grado(i)+  "Mrad");
         cd(string(cartelle(i)));
         cartella_dispositivo = estrazioneCartelle.getFileCartella(dispositivo);
 
         cd(string(cartella_dispositivo(1)));
         
-        file = "id-vgs.txt";
-        if(~exist(file , "file"))
-            file = "id_vgs.txt";
-        end
+        file = "gm.txt";
 
-        [vgs , id , ~] = EstrazioneDati.estrazione_dati_vgs(file , tipologia);
-        
-        %calcoliamo la gm alla massima vds
-        gm = gm_gds(id(: , end) , vgs);
-        
-        %prendiamo la massima gm
-        gm = max(gm);
+        gm = readmatrix(file);
+        %togliamo la vgs
+        gm = gm(: , 2:end);
+
+        %Per ogni valore di vds
+        for j = 1 : length(valori_vds)
+            %selezioniamo la vds, troviamo l'indice di vds
+            disp("      Vds = " + valori_vds(j) + "V");
+            indice_vds = (valori_vds - valori_vds(j)) == 0;
+            gm_i = gm(: , indice_vds);
+            % prendiamo la massima gm
+            gm_max = max(gm_i);
     
-        % se stiamo guardando i dati pre irraggiamento lo prendiamo come riferimento
-        if(i == 1)
-            gm_pre = gm;
-            delta(1) = 0;
-        else
-            delta(i) = (gm - gm_pre)*100/gm_pre;
-        end
+            % se stiamo guardando i dati pre irraggiamento lo prendiamo come riferimento
+            if(i == 1)
+                gm_pre = gm_max;
+                delta( j , 1) = 0;
+            else
+                delta(j , i) = (gm_max - gm_pre)*100/gm_pre;
+            end
 
+        end
+        
         cd ../..
     
     end
     
+    % creaimo la tabella
+    %entriamo nella cartella DeltaGm
+    if(~exist("DeltaGm\" + dispositivo , "dir"))
+        mkdir("DeltaGm\" + dispositivo);
+    end
+    cd("DeltaGm\" + dispositivo);
+    %componiamo la matrice da salvare
+    matrice = ["Vds" , (grado + "Mrad"); valori_vds' , delta];
+    writematrix(matrice , "Delta_gm_" + dispositivo+ ".xls");
+
+    % Plot variazioni
+    if(0)
     dispositivo = char(dispositivo);
 
     titolo = tipologia+"MOS $" + dispositivo(1:3)+"-"+"0.0"+ dispositivo(5:end) + "$" ;
@@ -45,6 +62,7 @@ function  delta_gm_percentuale(dispositivo) %dispositivo = "200-30";
     title(titolo, Interpreter="latex" , FontSize=12);
     ylabel("$ \% \Delta g_m$" , Interpreter="latex" , FontSize=12);
     xlabel("Dose Assorbita $[Mrad]$" , Interpreter="latex" , FontSize=12);
-    %yline(0 , "--");
+    yline(0 , "--");
     grid on 
+    end
 end
