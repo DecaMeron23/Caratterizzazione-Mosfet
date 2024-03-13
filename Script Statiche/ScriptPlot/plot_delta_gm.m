73850fbb0e59052e012439d089bdc36b667f524e
DecaMeron23 <e.meroni1@studenti.unibg.it>
2024-03-13 10:15:35 +0100

    Creazione funzione per fare i plot della delta gm

---
+++ Script Statiche/ScriptPlot/plot_delta_gm.m
@@ -0,0 +1,23 @@
+function plot_delta_gm(vds , tipologia , dispositivo) % vds = 0.45 , tipologia = "P" , dispositivo = "200-30"
+    % posizionarsi nella cartella contenente i file delta_gm
+    % Plot variazioni 
+    grado = [0 5 50 100 200 600 1000];
+    valori_vds = [0.15 , 0.30 , 0.45 , 0.6 , 0.75 , 0.9];
+    file = "Delta_gm_" + dispositivo + ".xls";
+    delta = readmatrix(file);
+    delta = delta(2:end , 2:end);
+        
+    indice_vds = (valori_vds == vds);
+    delta = delta(indice_vds , :);
+
+    figure;
+    dispositivo = char(dispositivo);
+    titolo = tipologia+"MOS $" + dispositivo(1:3)+"-"+"0.0"+ dispositivo(5:end) + "$" ;
+    plot(grado , delta , Marker="diamond" , LineWidth=1);    
+    ytickformat('percentage');
+    title(titolo, Interpreter="latex" , FontSize=12);
+    ylabel("$ \% \Delta g_m$" , Interpreter="latex" , FontSize=12);
+    xlabel("Dose Assorbita $[Mrad]$" , Interpreter="latex" , FontSize=12);
+    yline(0 , "--");
+    grid on
+end
