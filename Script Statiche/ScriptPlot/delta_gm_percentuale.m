f462511a4d519571e1ee5c6b791c13a26dbcbc18
DecaMeron23 <e.meroni1@studenti.unibg.it>
2024-03-13 10:16:36 +0100

    Modifica delta_gm_percentuale
    Aggiunto ciclo for che esegue per tutti i dispositivi il calcolo della delta gm percentuale

--- Script Statiche/ScriptPlot/delta_gm_percentuale.m
+++ Script Statiche/ScriptPlot/delta_gm_percentuale.m
@@ -1,68 +1,60 @@
-function  delta_gm_percentuale(dispositivo) %dispositivo = "200-30" vds = 0.45
+function  delta_gm_percentuale() %dispositivo = "200-30" vds = 0.45
 
-    [cartelle , tipologia]= estrazioneCartelle.getCartelle();
+    [cartelle]= estrazioneCartelle.getCartelle();
 
     grado = [0 5 50 100 200 600 1000];
     valori_vds = [0.15 , 0.30 , 0.45 , 0.6 , 0.75 , 0.9];
-    delta = zeros(6 , 7);
-    for i = 1: length(cartelle)
-        disp("Grado = " + grado(i)+  "Mrad");
-        cd(string(cartelle(i)));
-        cartella_dispositivo = estrazioneCartelle.getFileCartella(dispositivo);
+    nomi_dispositivi = ["100-30" , "100-60" , "100-180" , "200-30" , "200-60", "200-180" , "600-30" , "600-60" , "600-180"];
 
-        cd(string(cartella_dispositivo(1)));
-        
-        file = "gm.txt";
-
-        gm = readmatrix(file);
-        %togliamo la vgs
-        gm = gm(: , 2:end);
-
-        %Per ogni valore di vds
-        for j = 1 : length(valori_vds)
-            %selezioniamo la vds, troviamo l'indice di vds
-            disp("      Vds = " + valori_vds(j) + "V");
-            indice_vds = (valori_vds - valori_vds(j)) == 0;
-            gm_i = gm(: , indice_vds);
-            % prendiamo la massima gm
-            gm_max = max(gm_i);
+    for k = 1:length(nomi_dispositivi)
+        dispositivo = nomi_dispositivi(k);
+        disp("Dispositivo = "+ dispositivo);
+        delta = zeros(6 , 7);
+        for i = 1: length(cartelle)
+            disp("      Grado = " + grado(i)+  "Mrad");
+            cd(string(cartelle(i)));
+            cartella_dispositivo = estrazioneCartelle.getFileCartella(dispositivo);
     
-            % se stiamo guardando i dati pre irraggiamento lo prendiamo come riferimento
-            if(i == 1)
-                gm_pre = gm_max;
-                delta( j , 1) = 0;
-            else
-                delta(j , i) = (gm_max - gm_pre)*100/gm_pre;
+            cd(string(cartella_dispositivo(1)));
+            
+            file = "gm.txt";
+    
+            gm = readmatrix(file);
+            %togliamo la vgs
+            gm = gm(: , 2:end);
+    
+            %Per ogni valore di vds
+            for j = 1 : length(valori_vds)
+                %selezioniamo la vds, troviamo l'indice di vds
+                disp("              Vds = " + valori_vds(j) + "V");
+                indice_vds = (valori_vds - valori_vds(j)) == 0;
+                gm_i = gm(: , indice_vds);
+                % prendiamo la massima gm
+                gm_max = max(gm_i);
+        
+                % se stiamo guardando i dati pre irraggiamento lo prendiamo come riferimento
+                if(i == 1)
+                    gm_pre = gm_max;
+                    delta( j , 1) = 0;
+                else
+                    delta(j , i) = (gm_max - gm_pre)*100/gm_pre;
+                end
+    
             end
-
+            
+            cd ../..
+        
         end
         
-        cd ../..
-    
-    end
-    
-    % creaimo la tabella
-    %entriamo nella cartella DeltaGm
-    if(~exist("DeltaGm\" + dispositivo , "dir"))
-        mkdir("DeltaGm\" + dispositivo);
-    end
-    cd("DeltaGm\" + dispositivo);
-    %componiamo la matrice da salvare
-    matrice = ["Vds" , (grado + "Mrad"); valori_vds' , delta];
-    writematrix(matrice , "Delta_gm_" + dispositivo+ ".xls");
-
-    % Plot variazioni
-    if(0)
-    dispositivo = char(dispositivo);
-
-    titolo = tipologia+"MOS $" + dispositivo(1:3)+"-"+"0.0"+ dispositivo(5:end) + "$" ;
-
-    plot(grado , delta , Marker="diamond" , LineWidth=1);    
-    ytickformat('percentage');
-    title(titolo, Interpreter="latex" , FontSize=12);
-    ylabel("$ \% \Delta g_m$" , Interpreter="latex" , FontSize=12);
-    xlabel("Dose Assorbita $[Mrad]$" , Interpreter="latex" , FontSize=12);
-    yline(0 , "--");
-    grid on 
+        % creaimo la tabella
+        %entriamo nella cartella DeltaGm
+        if(~exist("DeltaGm", "dir"))
+            mkdir("DeltaGm\");
+        end
+        cd DeltaGm;
+        %componiamo la matrice da salvare
+        matrice = ["Vds" , (grado + "Mrad"); valori_vds' , delta];
+        writematrix(matrice , "Delta_gm_" + dispositivo+ ".xls");
+        cd ..
     end
 end
