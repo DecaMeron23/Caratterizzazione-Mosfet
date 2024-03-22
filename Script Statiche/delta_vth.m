a67e3e8415a8e6a11e22d193993a844c07424e19
DecaMeron23 <e.meroni1@studenti.unibg.it>
2024-03-22 18:52:50 +0100

    Update delta_vth.m
    eliminazione funzioni già presenti nella classe estrazioneCartelle

--- Script Statiche/delta_vth.m
+++ Script Statiche/delta_vth.m
@@ -14,7 +14,7 @@ function delta_vth(dispositivo)
     nomeDispositivo = "Chip"+ dispositivo(2) + upper(dispositivo(1)) + "MOS";
     
     % cartelle del dispositivo che ci interessa
-    cartelleDispostivo = getFileCartella(nomeDispositivo);
+    cartelleDispostivo = estrazioneCartelle.getFileCartella(nomeDispositivo);
 
     %se è vuota terminiamo
     if(isempty(cartelleDispostivo))
@@ -22,7 +22,7 @@ function delta_vth(dispositivo)
         return;
     end
 
-    cartelleDispostivo  = sortCartelleIrraggiamento(cartelleDispostivo);
+    cartelleDispostivo  = estrazioneCartelle.sortCartelleIrraggiamento(cartelleDispostivo);
     
 
     delta_FIT = {};
@@ -42,7 +42,7 @@ function delta_vth(dispositivo)
          if(exist(folder , "dir"))
             cd(folder);
                 grado{end+1} = gradoIrraggiamento(folder_main);
-                fileVth = getFileCartella("Vth");
+                fileVth = estrazioneCartelle.getFileCartella("Vth");
                 % estraiamo i dati
                 delta_FIT{end+1} = estraiVth(fileVth , "FIT");
                 delta_TCM{end+1} = estraiVth(fileVth , "TCM");
@@ -147,53 +147,6 @@ function grado = gradoIrraggiamento(nomeCartella)
         grado = nomeCartella;
         
         disp(grado);
-end
-
-% Funzione che prende tutti i file nella cartella, si può inserire un
-% parametro opzionale che sarà una striga che devono contenere i file
-% restituiti (so che non mi sono spiegato... però spero che scriverò il codice in modo da farlo capire)
-function fileInFolder = getFileCartella(varargin)
-
-            directory = dir();
-            fileInFolder = {directory.name};
-            fileInFolder(1:2) = []; % rimuovo . e ..
-
-            if(~isempty(varargin))
-                fileInFolder_controllati = {};
-                regola = string(varargin{1});
-                for file_i = fileInFolder
-                    file = string(file_i);
-                    if(contains(file , regola))
-                        fileInFolder_controllati{end+1} = file;
-                    end
-                end
-                fileInFolder = fileInFolder_controllati;
-            end
-end
-
-% sortiamo le cartelle con l'ordine pre 10M 50M 100M 200M 600M 1G
-function  cartelle_sort = sortCartelleIrraggiamento(cartelle)
-
-    cartelle_sort = {};
-    for i = cartelle
-        folder = string(i);
-        if(contains(folder , "5Mrad"))
-            cartelle_sort{2} = folder;
-        elseif(contains(folder , "50Mrad"))
-            cartelle_sort{3} = folder;
-        elseif(contains(folder , "100Mrad"))
-            cartelle_sort{4} = folder;
-        elseif(contains(folder , "200Mrad"))
-            cartelle_sort{5} = folder;
-        elseif(contains(folder , "600Mrad"))
-            cartelle_sort{6} = folder;
-        elseif(contains(folder , "1Grad"))
-            cartelle_sort{7} = folder;
-        else
-            cartelle_sort{1} = folder;
-        end
-
-    end
 end
 
 % funzione che prende un cell arry, di file .txt che conterranno le Vth per
